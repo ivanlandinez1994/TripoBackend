08e465387d83d726b09e1dcbc26088066c1a808d
ivanlandinez1994 <ivan.landinez.gallego@gmail.com>
2020-05-31 01:03:04 -0300

    cambios

--- routes/users.js
+++ routes/users.js
@@ -17,18 +17,25 @@ router.get('/', (req, res)=>{
 router.post('/add', (req,res)=>{
     const Usuario = new schema(req.body);
     Usuario.save() //guarda registro de la base de datos
-    .then((msg) => {
-        console.log(msg)
-        res.redirect('/users');
+    .then((user) => {
+        //res.redirect('/users');
+        res.send(`Se agrego el usuario ${user.nombre} correctamente`)
     })
     .catch(err=>console.log(chalk.red(err)))
 });
 
 router.delete('/delete/:id', (req,res)=>{
     const { id } = req.params;
+    var name;
+    schema.findById(id)
+    .then((user)=>{
+        name = user.nombre;
+    });
+
     schema.deleteOne({_id:id})
     .then(()=>{
-        res.redirect('/users');
+        //res.redirect('/users');
+        res.send(`El usuario ${name} fue eliminado`)
     })
     .catch(err => console.log(err));
 })
@@ -44,12 +51,12 @@ router.get('/update/:id', (req,res)=>{
     .catch(err => console.log(err));
 })
 
-
 router.put('/update/:id', (req,res)=>{
     const { id } = req.params;
     schema.updateOne({_id:id}, req.body)
     .then(()=>{
-        res.redirect('/users');
+        //res.redirect('/users');
+        res.send(`El usuario ${req.body.nombre} fue actualizado`)
     })
     .catch(err => console.log(err));
 })
@@ -60,7 +67,8 @@ router.put('/aprobar/:id', (req,res)=>{
     .then((user)=>{
         user.status = !user.status;
         user.save();
-        res.redirect('/users');
+        //res.redirect('/users');
+        res.send(`Estado del usuario: ${user.status?"Activo":"Inactivo"}`)
     })
     .catch(err => console.log(err));
 })
