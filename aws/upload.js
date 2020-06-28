a0618b8a7536bd2e1ea4619b080693f9d7d60d6c
ivanlandinez1994 <ivan.landinez.gallego@gmail.com>
2020-06-28 02:58:06 -0300

    ads

--- aws/upload.js
+++ aws/upload.js
@@ -26,7 +26,7 @@ const sendImage = async (filename) => {
             throw err;
         }
         return data;
-    }).promise()
+    }).promise().catch((err)=>{"No se pudo subir el archivo "+err})
 
     return rutaImagen.Location
 }
